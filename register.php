71e3a34a87b1e68bfa9f15984a8bec6e7a7ec0b4
christopher.magno <christopher.magno@firstmac.com.au>
2025-03-15 05:57:09 +0800

    fixed unique_id function

--- register.php
+++ register.php
@@ -1,6 +1,9 @@
 <?php
 
 include 'components/connect.php';
+function unique_id($length = 13) {
+   return substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, $length);
+}
 
 if(isset($_COOKIE['user_id'])){
    $user_id = $_COOKIE['user_id'];
